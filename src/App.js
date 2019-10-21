5a1f599879fbc8224e6704eac2efc3ea1ed1f269
Jerry <jerryb.adeleye@gmail.com>
2019-10-21 22:32:41 +0100

    chore: styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo2.png';
+import pluralsight from './pluralsight.png'
+import google from './google_logo.png'
+import alc from './alc.png'
+import twitter from './twitter.png'
+
 import './App.css';
 
 function App() {
@@ -7,17 +12,35 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>ALC 4.0 Phase II Cloud Challenge I</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React App on GKE
         </p>
-        <a
+        <p>
+          Deployed by&nbsp;
+           <a
+          className="App-link"
+          href="https://twitter.com/jerry_adeleye"
+          target="_blank"
+          rel="noopener noreferrer">
+          Jerry Adeleye
+          </a>
+        </p>
+        <p>
+          Checkout the source code &nbsp; 
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          rel="noopener noreferrer">
+          here
+          </a>
+        </p>
+        <div className="Images-div">
+        <img src={pluralsight} className="pluralsight-image" alt="pluralsight"/>
+        <img src={google} className="google-image" alt="google"/>
+        <img src={alc} className="google-image" alt="alc"/>
+        </div>
       </header>
     </div>
   );
